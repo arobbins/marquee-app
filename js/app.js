7b78381d24242ded9ff36ffdc8dd3d583dbc8c51
arobbins <arobbins@simpleblend.net>
2014-02-10 07:24:00 -0600

    Added ng-repeat and controller testing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 var mapp = angular.module('mapp', []);
+
+
+function first($scope){
+	$scope.marquees = {
+		"Triple A": {
+			contentkey: "aaa612",
+			template: "Left",
+			theme: "Dark"
+		},
+		"Amtrak Cascades": {
+			contentkey: "cascades0214",
+			template: "Left",
+			theme: "Dark"
+		},
+		"Double Days": {
+			contentkey: "doubledays2014",
+			template: "Left",
+			theme: "Dark"
+		}
+	};
+}
