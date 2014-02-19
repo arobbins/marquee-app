093b8dd0f212bef89dee0e76bd3c047aa9e649b2
arobbins <arobbins@simpleblend.net>
2014-02-18 19:48:16 -0600

    Added ng functionality, added more styles

--- js/app.js
+++ js/app.js
@@ -1,23 +1,6 @@
 var app = angular.module('app', []);
 
+function m($scope){
+	return $scope.m;
 
-function first($scope){
-	$scope.property1 = "Prop1",
-	$scope.marquees = {
-		"Triple A": {
-			contentkey: "aaa612",
-			template: "Left",
-			theme: "Dark"
-		},
-		"Amtrak Cascades": {
-			contentkey: "cascades0214",
-			template: "Left",
-			theme: "Dark"
-		},
-		"Double Days": {
-			contentkey: "doubledays2014",
-			template: "Left",
-			theme: "Dark"
-		}
-	};
 }
