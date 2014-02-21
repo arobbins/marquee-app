2e072c1bb3aaf0194c1b43080616004e0d3e73b5
arobbins <arobbins@simpleblend.net>
2014-02-21 13:29:56 -0600

    Removed SASS, added LESS. Partitioned views.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,29 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap']);
 
-function m($scope){
-	return $scope;
+app.controller('m', ['$scope', function($scope) {
+	$scope.title = "Marquee #1";
+	$scope.template = "tpl-left";
+}]);
 
+
+function AccordionDemoCtrl($scope) {
+  $scope.oneAtATime = true;
+
+  $scope.groups = [
+    {
+      title: "Dynamic Group Header - 1",
+      content: "Dynamic Group Body - 1"
+    },
+    {
+      title: "Dynamic Group Header - 2",
+      content: "Dynamic Group Body - 2"
+    }
+  ];
+
+  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+  $scope.addItem = function() {
+    var newItemNo = $scope.items.length + 1;
+    $scope.items.push('Item ' + newItemNo);
+  };
 }
