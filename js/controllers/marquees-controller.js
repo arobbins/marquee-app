5b31938b3308fb84192c00f77b958701d39e13e2
arobbins <arobbins@simpleblend.net>
2014-04-30 17:11:41 -0500

    Added search text function

--- js/controllers/marquees-controller.js
+++ js/controllers/marquees-controller.js
@@ -1,6 +1,36 @@
 app.controller('MarqueesCtrl', function($scope, $stateParams, ngDialog, MarqueeData) {
 
+	$scope.searchText = function(str){
+
+		// Declaring our string and patterns
+		var patternSmall = /s:(.*)b:/;
+		var patternBig = /b:(.*)/;
+			
+		// Matching the small text
+		var matchSmall = patternSmall.exec(str);
+		var smalltext = matchSmall[1];
+
+		// Matching the big text
+		var matchBig = patternBig.exec(str);
+		var bigtext = matchBig[1];
+
+		// Modifying strings with added HTML elements
+		var smalltext = "<span>" + smalltext + "</span>";
+		var bigtext = "<b>" + bigtext + "</b>";
+
+		// return [smalltext, bigtext];
+		return {
+			smalltext: smalltext, 
+			bigtext: bigtext
+    	}; 
+		// console.log(smalltext);
+		// console.log(bigtext);
+	}
+
+	searchText($scope.m.marquees[0].header);
+	
 	$scope.isCollapsed = true;
+	$scope.disabled = "disabled";
 
 	// Storing all of our marquees in this m object when we search
 	$scope.search = {
@@ -8,12 +38,12 @@ app.controller('MarqueesCtrl', function($scope, $stateParams, ngDialog, MarqueeD
 	    release: ''
 	};
 
-	$scope.disabled = "disabled";
-
 	// Setting the results of making the AJAX call to our 'm' object.
 	MarqueeData.getMarqueeData(function(results) {
 		return $scope.m = results;
 	});
+
+	
 
 	$scope.checkRelease = function(){
 		if($scope.search.m){
