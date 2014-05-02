5379f00bdbf251e805137fa08148e62d76915e60
arobbins <arobbins@simpleblend.net>
2014-05-02 16:42:19 -0500

    Added header functionality

--- js/controllers/marquees-controller.js
+++ js/controllers/marquees-controller.js
@@ -1,47 +1,47 @@
 app.controller('MarqueesCtrl', function($scope, $stateParams, ngDialog, MarqueeData) {
 
-	$scope.searchText = function(str){
+	// $scope.searchText = function(str){
 
-		// Declaring our string and patterns
-		var patternSmall = /s:(.*)b:/;
-		var patternBig = /b:(.*)/;
+	// 	// Declaring our string and patterns
+	// 	var patternSmall = /s:(.*)b:/;
+	// 	var patternBig = /b:(.*)/;
 
-		// Matching the small text
-		var matchSmall = patternSmall.exec(str);
-		var smalltext = matchSmall[1];
+	// 	// Matching the small text
+	// 	var matchSmall = patternSmall.exec(str);
+	// 	var smalltext = matchSmall[1];
 
-		// Works but not sure of order
-		"s:This is an example b:that is so cool".replace(/s:(.*)b:(.*)/, function(match, $1, $2, offset, string){
-			return '<span>' + $1 + '</span>' + '<b>' + $2 + '</b>';
-		});
-
-
-		/^.*?\bs:(.*)\b.*?\bb:(.*)\b.*?$/m.exec("b:This is an example s:that is so cool");
-
-		var str = 's: This is an example b: This is also an example';
-		var reg = /[s:](.*)[b:]|[s:]/;
-
-		reg.exec(str);
-
-		alert(str.match(reg)[0]);
-
-		// Matching the big text
-		var matchBig = patternBig.exec(str);
-		var bigtext = matchBig[1];
-
-		// Modifying strings with added HTML elements
-		var smalltext = "<span>" + smalltext + "</span>";
-		var bigtext = "<b>" + bigtext + "</b>";
+	// 	// Works but not sure of order
+	// 	"s:This is an example b:that is so cool".replace(/s:(.*)b:(.*)/, function(match, $1, $2, offset, string){
+	// 		return '<span>' + $1 + '</span>' + '<b>' + $2 + '</b>';
+	// 	});
 
 
-		// return [smalltext, bigtext];
-		return {
-			smalltext: smalltext,
-			bigtext: bigtext
-    	};
-		// console.log(smalltext);
-		// console.log(bigtext);
-	}
+	// 	/^.*?\bs:(.*)\b.*?\bb:(.*)\b.*?$/m.exec("b:This is an example s:that is so cool");
+
+	// 	var str = 's: This is an example b: This is also an example';
+	// 	var reg = /[s:](.*)[b:]|[s:]/;
+
+	// 	reg.exec(str);
+
+	// 	alert(str.match(reg)[0]);
+
+	// 	// Matching the big text
+	// 	var matchBig = patternBig.exec(str);
+	// 	var bigtext = matchBig[1];
+
+	// 	// Modifying strings with added HTML elements
+	// 	var smalltext = "<span>" + smalltext + "</span>";
+	// 	var bigtext = "<b>" + bigtext + "</b>";
+
+
+	// 	// return [smalltext, bigtext];
+	// 	return {
+	// 		smalltext: smalltext,
+	// 		bigtext: bigtext
+ //    	};
+	// 	// console.log(smalltext);
+	// 	// console.log(bigtext);
+	// }
 
 	// searchText($scope.m.marquees[0].header);
 
@@ -59,9 +59,16 @@ app.controller('MarqueesCtrl', function($scope, $stateParams, ngDialog, MarqueeD
 		return $scope.m = results;
 	});
 
+	$scope.addContent = function(marquee, input1, input2) {
 
+		input1 = "<span>" + input1 + "</span>";
+		input2 = "<b>" + input2 + "</b>";
 
-	$scope.checkRelease = function(){
+		// console.log(input1 + input2);
+		return marquee.header = input1 + input2;
+	};
+
+	$scope.checkRelease = function() {
 		if($scope.search.m){
 			$scope.onrelease = false;
 		} else {
@@ -78,29 +85,29 @@ app.controller('MarqueesCtrl', function($scope, $stateParams, ngDialog, MarqueeD
 		});
 	};
 
-	$scope.showAllMarquees = function(){
+	$scope.showAllMarquees = function() {
 		MarqueeData.getMarqueeData(function(results) {
 			return $scope.m.marquees = results.marquees;
 		});
 	};
 
-	$scope.removeMarquee = function(){
+	$scope.removeMarquee = function() {
 
 	};
 
-	$scope.addMarquee = function(){
+	$scope.addMarquee = function() {
 
 	};
 
 	// Collapse and expand
-	$scope.toggleMarquees = function(){
+	$scope.toggleMarquees = function() {
 		$scope.m.marquees.forEach(function(marquee) {
 			return marquee.toggled = !marquee.toggled;
 		});
 	};
 
 	// Filter marquees based on release
-	$scope.filterMarquees = function(releaseDate){
+	$scope.filterMarquees = function(releaseDate) {
 		$scope.search.m = '';
 		$scope.disabled = '';
 		MarqueeData.getMarqueeData(function(results) {
